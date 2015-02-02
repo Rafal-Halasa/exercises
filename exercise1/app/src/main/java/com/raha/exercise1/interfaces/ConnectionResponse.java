c7320012ad957c05fd14253a2b620c610a8bd17a
raha <rafal.halasa>
2015-02-02 12:31:52 +0100

    clean repo

--- exercise1/app/src/main/java/com/raha/exercise1/interfaces/ConnectionResponse.java
+++
@@ -1,8 +0,0 @@
-package com.raha.exercise1.interfaces;
-
-/**
- * Created by raha on 2015-01-29.
- */
-public interface ConnectionResponse {
-    public Integer getStatusAndUrl(Boolean status,String url);
-}
